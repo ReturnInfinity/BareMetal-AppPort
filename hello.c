1f827c8fc11901d7bcc094da2ebfec4c2373496d
Ian Seyler <ian.seyler@returninfinity.com>
2026-08-15 17:42:27 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,7 @@
-// helloc.c -- Output a 'hello world' message via musl's printf,
-// proving the musl -> posix_shim -> libBareMetal path works end to end.
-// build with build-app.sh
-
 #include <stdio.h>
 
 int main()
 {
-	printf("Hello, World!\n");
-	return 0;
+    printf("Hello from C!\n");
+    return 0;
 }
